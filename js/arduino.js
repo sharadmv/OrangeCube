184714a1d103bddb518f1d8c76cc84af03b62d68
Sharad Vikram <sharad.vikram@gmail.com>
2012-04-14 15:37:39 -0700

    mutliple sensors

--- js/arduino.js
+++ js/arduino.js
@@ -2,16 +2,42 @@
 var sys = require('sys');
 var sp = require('serialport');
 var SerialPort = sp.SerialPort;
-var serialPort = new SerialPort("/dev/ttyUSB0",{
-    parser:sp.parsers.readline('\n')
-});
-serialPort.on('data',function(data){
-  var temp = data.toString('ascii').trim().match(/[0-9]+/g);
-  if (temp != null){
-    console.log(temp[0]);
-  }
-});
-spawn = require('child_process').spawn;
+var spawn = require('child_process').spawn;
+var Sensor = function(port,num,onData,onStartTouch, onEndTouch){
+  var touch = new Array(num);
+  var serialPort = new SerialPort(port,{
+      parser:sp.parsers.readline('\n')
+  });
+  serialPort.on('data',function(data){
+    var temp = data.toString('ascii').trim().match(/[0-9]+/g);
+    if (temp){
+      for (var i in temp) {
+        if (temp[i] > 200){
+          if (touch[i]===undefined){
+            touch[i] = true;
+          }
+          if (touch[i] == false){
+            touch[i] = true;
+            onStartTouch(i);
+          }
+        } else {
+          if (touch[i]===undefined){
+            touch[i] = false;
+          }
+          if (touch[i] == true){
+            touch[i] = false;
+            onEndTouch(i);
+          }
+        }
+      }
+      onData(temp);
+    }
+  });
+}
+var serialPort = new Sensor('/dev/ttyUSB0',2,function(num){
+}, function(num){
+  console.log("STARTED TOUCH "+num);
+}, function(num){console.log("ENDED TOUCH "+num)});
 var mouse = {
   move:function(x,y){
     var mouse = spawn('./mouse.py',[x,y]);

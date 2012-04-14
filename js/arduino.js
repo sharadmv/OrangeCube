478d3cd6285d1ccda2f6f03fcb670349e46bd6b7
Sharad Vikram <sharad.vikram@gmail.com>
2012-04-14 16:07:02 -0700

    added clicking functionality

--- js/arduino.js
+++ js/arduino.js
@@ -2,6 +2,7 @@
 var sp = require('serialport');
 var SerialPort = sp.SerialPort;
 var spawn = require('child_process').spawn;
+var TOUCH_THRESHOLD = 200;
 var Sensor = function(port,num,onData,onStartTouch, onEndTouch){
   var started = false;
   var touch = new Array(num);
@@ -16,7 +17,7 @@ var Sensor = function(port,num,onData,onStartTouch, onEndTouch){
     var temp = data.toString('ascii').trim().match(/[0-9]+/g);
     if (temp){
       for (var i in temp) {
-        if (temp[i] > 200){
+        if (temp[i] > TOUCH_THRESHOLD){
           if (touch[i]===undefined){
             touch[i] = true;
           }
@@ -41,10 +42,20 @@ var Sensor = function(port,num,onData,onStartTouch, onEndTouch){
 var serialPort = new Sensor('/dev/ttyUSB0',2,function(num){
 }, function(num){
   console.log("STARTED TOUCH "+num);
+  if (num == 1){
+    mouse.click(-1,-1,"right");
+  }else if (num==0){
+    mouse.click(-1,-1,"left");
+  }
 }, function(num){console.log("ENDED TOUCH "+num)});
 var mouse = {
   move:function(x,y){
     var mouse = spawn('./mouse.py',[x,y]);
+  },
+  click:function(x,y,type){
+    console.log(x,y,type+"click")
+    var mouse = spawn('./mouse.py',[x,y,type+"click"]);
+    //mouse.stdout.on('data',function(data){console.log(data.toString('ascii'))});
   }
 }
 var curX = 101,curY = 101;

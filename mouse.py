e75ff454c8a6df0c676cac97274554523923cd09
Sharad Vikram <sharad.vikram@gmail.com>
2012-04-14 14:09:52 -0700

    added mouse test code

--- mouse.py
+++ mouse.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python
-# import the module
 from pymouse import PyMouse
-
+import sys
 # instantiate an mouse object
 m = PyMouse()
 
+x = int(sys.argv[1])
+y = int(sys.argv[2])
 # move the mouse to int x and int y (these are absolute positions)
-m.move(200, 200)
-
-# click works about the same, except for int button possible values are 1: left, 2: middle, 3: right
-m.click(500, 300, 1)
-
-# get the screen size
-m.screen_size()
-# (1024, 768)
-
-# get the mouse position
-m.position()
-# (500, 300)
-
+m.move(x,y)
+print("Mouse moved to "+str(x)+", "+str(y));
--- test.js
+++ test.js
@@ -1,2 +1,27 @@
+#!/usr/local/bin/node
 var SerialPort = require('serialport').SerialPort;
-console.log(SerialPort);
+spawn = require('child_process').spawn;
+var x =  1300; var y = 700;
+var mouse = {
+  move:function(x,y){
+    spawn('./mouse.py',[x,y]);
+  }
+}
+var curX = 101,curY = 101;
+mouse.move(curX,curY);
+var vX = 2, vY = 2;
+setInterval(function(){
+  if (curX == x){
+    vX *=-1; 
+  } else if (curX == 100){
+    vX *= -1;
+  }
+  if (curY == y){
+    vY *= -1; 
+  } else if (curY == 100){
+    vY *= -1;
+  }
+  curX += vX;
+  curY += vY;
+  mouse.move(curX,curY);
+},100);

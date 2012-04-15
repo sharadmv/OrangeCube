cc05b91ca89dacdc2b3c60a6f1ae0ff6a0c3a069
Gerald <geraldgfong@gmail.com>
2012-04-15 08:42:48 -0700

    removed the boxes

--- chrome_ext/test.js
+++ chrome_ext/test.js
@@ -2,9 +2,9 @@ var bridge = new Bridge( {host: "cloud2.flotype.com", port: 8091, apiKey: "abcde
 bridge.connect();
 
 $("body").append("<div style='width: 100%;height: 100%; position: fixed; z-index:100!important; top: 0; left: 0;'id='orange-frame'></div>");
-for(var x = 0; x < 9; x++) {
-  var color = "rgba(" + Math.floor(Math.random() * 255 )+ ", " + Math.floor(Math.random() * 255) + ", " + Math.floor(Math.random() * 255) + ", .4)";
-  $("#orange-frame").append("<div style='z-index:100!important; position:relative; background-color: "+color+"; width: 33.3333333333333%; height: 33.33333333333%; float: left;' class='frame-ninth' id='frame"+x+"'></div>");
+for(var x = 0; x < 4; x++) {
+  var color = "rgba(" + Math.floor(Math.random() * 255 )+ ", " + Math.floor(Math.random() * 255) + ", " + Math.floor(Math.random() * 255) + ", .0)";
+  $("#orange-frame").append("<div style='z-index:100!important; position:relative; background-color: "+color+"; width: 50%; height: 50%; float: left;' class='frame-ninth' id='frame"+x+"'></div>");
 }
 
 var left = 0;
@@ -30,46 +30,27 @@ var clickFrame = function(frameNum) {
 //setInterval(function() {clickFrame(Math.floor(Math.random() * 9))}, 2000);
 
 var shiftFrame = function(left, top, width, height) {
-  //$("#orange-frame").offset({left: left, top: top}).width(width).height(height);
-  $("#orange-frame").animate({left: left, top: top, width: width, height: height});
+  $("#orange-frame").offset({left: left, top: top}).width(width).height(height);
+  //$("#orange-frame").animate({left: left, top: top, width: width, height: height});
 }
 
 var updateFrame = function() {
+  console.log(top1);
+  
   shiftFrame(left, top1, width, height);
 }
 
 var scrollDown = function() {
-  $("body").animate({scrollTop: '+=50'});
-  top1 += 50;
-  if( top1 + height > $("body").height()) {
-    top1 = $("body").height();
-  }
-
-  updateFrame();
+  $("body").animate({scrollTop: '+=100'});
 }
 var scrollUp = function() {
-  $("body").animate({scrollTop: '-=50'});
-  top1 -= 50;
-  if( top1 < 0) {
-    top1 = 0;
-  }
-  updateFrame();
+  $("body").animate({scrollTop: '-=100'});
 }
 var scrollLeft = function() {
-  $("body").animate({scrollLeft: '-=50'});
-  left -= 50;
-  if( left < 0) {
-    left = 0;
-  }
-  updateFrame();
+  $("body").animate({scrollLeft: '-=100'});
 }
 var scrollRight = function() {
-  $("body").animate({scrollLeft: '+=50'});
-  left += 50;
-  if( left + $(window).width() > $("body").width()) {
-    left = left - $(window).width();
-  }
-  updateFrame();
+  $("body").animate({scrollLeft: '+=100'});
 }
 
 
@@ -106,9 +87,8 @@ function barrelRollRight() {
   }, 3600);
 }
 
-
 var remoteFuncs = {
-  // pass 0 to 8 corresponding to the 9 frames left to right, top to bottom
+  // pass 0 to 3 corresponding to the 4 frames left to right, top to bottom
   clickFrame: clickFrame,
   scrollDown: scrollDown,
   scrollUp: scrollUp,
@@ -119,7 +99,7 @@ var remoteFuncs = {
 }
 
 
+
 bridge.ready(function() {
   bridge.publishService("log", remoteFuncs);
-  console.log("HI");
 });

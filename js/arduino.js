56b6b825aebc68757c606b1e52ea71d6bbc1bdb7
Sharad Vikram <sharad.vikram@gmail.com>
2012-04-15 01:55:25 -0700

    weird crappy hacky commit

--- js/arduino.js
+++ js/arduino.js
@@ -4,122 +4,129 @@ var SerialPort = sp.SerialPort;
 var spawn = require('child_process').spawn;
 var TOUCH_THRESHOLD = 200;
 var CALIBRATION_TIME = 5000;
-var PROXIMITY_AVG_LENGTH = 30;
+var PROXIMITY_AVG_LENGTH = 10;
 var NUM_SENSORS = 1;
 var net = require('net');
-var Sensor = function(port,num,onData,onStartTouch, onEndTouch, onProximity){
-  var started = false;
-  var dataCount = 0;
-  var calibration = true; var touch = new Array(num);
-  var calibs = new Array(num);
-  var proximity = new Array(num);
-  var serialPort = new SerialPort(port,{
-      parser:sp.parsers.readline('\n')
-  });
-  serialPort.on('data',function(data){
-    if (!started) {
-      setTimeout(function(){calibration = false;
-        console.log("Ending sensor calibration");
-        for (var i in calibs){
-          calibs[i] = calibs[i]/dataCount;
-        }
-      },CALIBRATION_TIME);
-      console.log("Started listening on serial port: "+port+", "+num+" sensors");
-      console.log("Starting sensor calibration");
-      started = true;
-    }
-    var temp = data.toString('ascii').trim().match(/[0-9]+/g);
-    var calibrated = data.toString('ascii').trim().match(/[0-9]+/g);
-    if (temp){
-      dataCount++;
-      for (var i in temp) {
-        if (calibration){
-          if (calibs[i] === undefined){
-            calibs[i] = parseInt(temp[i]);
+var HOST = "localhost";
+var PORT = 1234;
+var client = new net.Socket();
+client.connect(PORT,HOST, function(){
+  client.on('close',function(){console.log(arguments)});
+  client.write("move 400 400");
+  client.write("move 600 400");
+  var Sensor = function(port,num,onData,onStartTouch, onEndTouch, onProximity){
+    var started = false;
+    var dataCount = 0;
+    var calibration = true; var touch = new Array(num);
+    var calibs = new Array(num);
+    var proximity = new Array(num);
+    var serialPort = new SerialPort(port,{
+        parser:sp.parsers.readline('\n')
+    });
+    serialPort.on('data',function(data){
+      if (!started) {
+        setTimeout(function(){calibration = false;
+          console.log("Ending sensor calibration");
+          for (var i in calibs){
+            calibs[i] = calibs[i]/dataCount;
+          }
+        },CALIBRATION_TIME);
+        console.log("Started listening on serial port: "+port+", "+num+" sensors");
+        console.log("Starting sensor calibration");
+        started = true;
+      }
+      var temp = data.toString('ascii').trim().match(/[0-9]+/g);
+      var calibrated = data.toString('ascii').trim().match(/[0-9]+/g);
+      if (temp){
+        dataCount++;
+        for (var i in temp) {
+          if (calibration){
+            if (calibs[i] === undefined){
+              calibs[i] = parseInt(temp[i]);
+            } else {
+              calibs[i]+= parseInt(temp[i]);
+            }
           } else {
-            calibs[i]+= parseInt(temp[i]);
+            calibrated[i] = temp[i]-(calibs[i]);
           }
-        } else {
-          calibrated[i] = temp[i]-(calibs[i]);
+          if (temp[i] > TOUCH_THRESHOLD){
+            if (touch[i]===undefined){
+              touch[i] = true;
+            }
+            if (touch[i] == false){
+              touch[i] = true;
+              onStartTouch(i);
+            }
+          } else {
+            if (touch[i]===undefined){
+              touch[i] = false;
+            }
+            if (touch[i] == true){
+              touch[i] = false;
+              onEndTouch(i);
+            }
+          }
+          if (proximity[i] === undefined){
+            proximity[i] = [];
+            proximity[i].push(parseInt(calibrated[i]));
+          } else {
+            if (proximity[i].length > PROXIMITY_AVG_LENGTH){
+              proximity[i].shift(); 
+            }
+            proximity[i].push(parseInt(calibrated[i]));
+          }
         }
-        if (temp[i] > TOUCH_THRESHOLD){
-          if (touch[i]===undefined){
-            touch[i] = true;
-          }
-          if (touch[i] == false){
-            touch[i] = true;
-            onStartTouch(i);
-          }
-        } else {
-          if (touch[i]===undefined){
-            touch[i] = false;
-          }
-          if (touch[i] == true){
-            touch[i] = false;
-            onEndTouch(i);
-          }
-        }
-        if (proximity[i] === undefined){
-          proximity[i] = [];
-          proximity[i].push(parseInt(calibrated[i]));
-        } else {
-          if (proximity[i].length > PROXIMITY_AVG_LENGTH){
-            proximity[i].shift(); 
-          }
-          proximity[i].push(parseInt(calibrated[i]));
+        if (!calibration){
+          onData(calibrated);
+          onProximity(proximity.map(function(e){var x = average(e);if (x<=1){x = 1};return x}));
         }
       }
-      if (!calibration){
-        onData(calibrated);
-        onProximity(proximity.map(function(e){var x = average(e);if (x<=1){x = 1};return x}));
-      }
-    }
-  });
+    });
   }
   var serialPort = new Sensor('/dev/ttyUSB0',2,function(num){
-    
+
     //console.log(num.join('\t'));
 }, function(num){
- // console.log("STARTED TOUCH "+num);
-/*   if (num == 1){
+  // console.log("STARTED TOUCH "+num);
+  /*   if (num == 1){
    mouse.click(-1,-1,"right");
   }else if (num==0){
   mouse.click(-1,-1,"left");
 }
-  */
+*/
   }, function(num){/*console.log("ENDED TOUCH "+num)*/},
   function(proximities){
     var c = [{x:1,y:1},{x:2,y:2}]; 
     /*for (var i=0;i<=4;i++){
-      if (c[i]===undefined){
-        c[i] = [];
-      }
-      for (var j=0;j<=4;j++){
-        c.push({x:j+1, y:j+1});
-      }
-      }*/
-    var d = [];
-    //console.log(proximities);
-    for (var i in proximities){
-      d[i] = proximities[i];
-    }
-    var avg = {x:0,y:0};
-    var length = 0;
-    for (var i=0;i<2;i++){
-      avg.x += d[i]*c[i].x;
-      avg.y += d[i]*c[i].y;
-      length += d[i];
-    }
-    //console.log(avg.x/length*500,100);
-    mouse.move(Math.floor(avg.x/length*500),100);
-    //console.log(proximities.map(function(e){return Math.floor(e)}).join('\t'));
+     if (c[i]===undefined){
+     c[i] = [];
+  }
+  for (var j=0;j<=4;j++){
+  c.push({x:j+1, y:j+1});
+  }
+  }*/
+  var d = [];
+  //console.log(proximities);
+  for (var i in proximities){
+    d[i] = proximities[i];
+  }
+  var avg = {x:0,y:0};
+  var length = 0;
+  for (var i=0;i<2;i++){
+    avg.x += d[i]*c[i].x;
+    avg.y += d[i]*c[i].y;
+    length += d[i];
+  }
+  //console.log(avg.x/length*500,100);
+  mouse.move(Math.floor(avg.x/length*500),100);
+  //console.log(proximities.map(function(e){return Math.floor(e)}).join('\t'));
   });
   var mouse = {
     move:function(x,y){
-      var mouse = spawn('java',['Mouse',x,y]);
+      client.write("move "+x+" "+y);
     },
     click:function(x,y,type){
-//      console.log(x,y,type+"click")
+      //      console.log(x,y,type+"click")
       var mouse = spawn('./mouse.py',[x,y,type+"click"]);
       //mouse.stdout.on('data',function(data){console.log(data.toString('ascii'))});
     }
@@ -136,4 +143,4 @@ var Sensor = function(port,num,onData,onStartTouch, onEndTouch, onProximity){
     }
     return av/cnt;
   }
-
+});
--- js/test.js
+++ js/test.js
@@ -4,7 +4,7 @@ var PORT = 1234;
 var client = new net.Socket();
 client.connect(PORT,HOST, function(){
   console.log("Connected");
-  client.write("100 100");
+  client.write("move 100 100");
   console.log("WROTE");
   client.on('data',function(data){
     console.log(data.toString());
